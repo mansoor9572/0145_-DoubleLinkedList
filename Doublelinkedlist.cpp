e9b96619c5f6df4a9c9e8f11d80e20e58ca826de
mansoor <t.alyamani95@gmail.com>
2025-05-13 10:00:54 +0700

    delacrasi noMhs dan name untuk menapung data

---
+++ Doublelinkedlist.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+//membuat structur untuk object double linked list 
+struct  Node 
+{
+   // delacrasi noMhs dan name untuk menapung data 
+   int noMhs;
+   string name ;
+   
+};
+
